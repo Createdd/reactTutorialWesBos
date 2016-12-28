47af5dda76c98450cbb9c3303e362d60b7749ea0
Daniel Deutsch <deudan1010@gmail.com>
2016-12-28 16:05:34 +0100

    first component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import React from 'react';
+import { render } from 'react-dom';
+
+class Storepicker extends React.Component {
+  render() {
+    return <p>Working</p>
+  }
+}
+render(<Storepicker/>, document.querySelector('#main'));
