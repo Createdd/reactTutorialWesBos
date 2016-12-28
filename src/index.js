1de3d3dd4d501b8e7b1aaa4ae05ec6652342190d
Daniel Deutsch <deudan1010@gmail.com>
2016-12-28 16:29:44 +0100

    add css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import './css/style.css';
 import Storepicker from './components/Storepicker';
 
 render(<Storepicker/>, document.querySelector('#main'));
