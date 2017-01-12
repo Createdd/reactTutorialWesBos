a4fa0bf626ee81ceb41d3faf2b7818425422216a
Daniel Deutsch <deudan1010@gmail.com>
2017-01-12 13:57:41 +0100

    events binding method

--- src/components/Storepicker.js
+++ src/components/Storepicker.js
@@ -2,19 +2,14 @@ import React from 'react';
 import { getFunName } from '../helpers'
 
 class Storepicker extends React.Component {
-  constructor(){
-    super();
-    this.goToStore = this.goToStore.bind(this);
-  }
-
   goToStore(event) {
     event.preventDefault();
-    console.log(this);
+    console.log(this.storeInput);
   }
 
   render() {
     return (
-        <form className="store-selector" onSubmit={this.goToStore}>
+        <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
           <h1>Choose A Store</h1>
           <input type="text" required placeholder="Store Name"
             defaultValue={getFunName()}
