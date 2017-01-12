0024d10017e206947b9eca982bfd3947faf7ef4d
Daniel Deutsch <deudan1010@gmail.com>
2017-01-12 13:45:49 +0100

    events preventDefault

--- src/components/Storepicker.js
+++ src/components/Storepicker.js
@@ -2,9 +2,14 @@ import React from 'react';
 import { getFunName } from '../helpers'
 
 class Storepicker extends React.Component {
+  goToStore(event) {
+    event.preventDefault();
+    console.log('logging');
+  }
+
   render() {
     return (
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
           <h1>Choose A Store</h1>
           <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
           <button type="submit">Visit -></button>
