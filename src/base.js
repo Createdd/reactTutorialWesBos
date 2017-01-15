9dfc4f263fbc56c648caf2d91e050e5e5fb418e9
Daniel Deutsch <deudan1010@gmail.com>
2017-01-15 11:32:06 +0100

    add base.js for firebase

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyCArGpNJZj_1OeqEV8lVQGVFfo2FBfp3to",
+  authDomain: "catch-of-the-day-ddcs.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-ddcs.firebaseio.com"
+});
+
+export default base;
